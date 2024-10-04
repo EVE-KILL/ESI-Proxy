e1112c824f742a655329e163ddcf4c27a202cb47
Michael Karbowiak <michael@karbowiak.dk>
2024-10-04 21:58:53 +0200

    Handle POST, DELETE, UPDATE etc. requests but bypass the cache

--- src/proxy/esi.go
+++ src/proxy/esi.go
@@ -49,26 +49,23 @@ func (rc *responseCapture) WriteHeader(statusCode int) {
 
 func RequestHandler(proxy *httputil.ReverseProxy, url *url.URL, endpoint string, rateLimiter *helpers.RateLimiter, cache *helpers.Cache, requestQueue *helpers.RequestQueue) func(http.ResponseWriter, *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
-		if r.Method != http.MethodGet {
-			proxy.ServeHTTP(w, r)
-			return
-		}
-
 		cacheKey := helpers.GenerateCacheKey(r.URL.String(), r.Header.Get("Authorization"))
-		if cachedResponse, found := cache.Get(cacheKey); found {
-			// Always return cached data if it exists
-			for key, values := range cachedResponse.Headers {
-				for _, value := range values {
-					w.Header().Add(key, value)
+		if r.Method == http.MethodGet {
+			if cachedResponse, found := cache.Get(cacheKey); found {
+				// Always return cached data if it exists
+				for key, values := range cachedResponse.Headers {
+					for _, value := range values {
+						w.Header().Add(key, value)
+					}
 				}
+				w.Header().Set("X-Proxy-Cache", "HIT")
+				w.WriteHeader(cachedResponse.Status)
+				w.Write(cachedResponse.Body)
+				return
 			}
-			w.Header().Set("X-Proxy-Cache", "HIT")
-			w.WriteHeader(cachedResponse.Status)
-			w.Write(cachedResponse.Body)
-			return
-		}
 
-		w.Header().Set("X-Proxy-Cache", "MISS")
+			w.Header().Set("X-Proxy-Cache", "MISS")
+		}
 
 		if backoff := rateLimiter.ShouldBackoff(); backoff > 0 {
 			requestQueue.Enqueue(w, r)
@@ -90,8 +87,8 @@ func RequestHandler(proxy *httputil.ReverseProxy, url *url.URL, endpoint string,
 		rc := &responseCapture{ResponseWriter: w, headers: make(http.Header)}
 		proxy.ServeHTTP(rc, r)
 
-		// Cache the response if it's a 200 OK or 304 Not Modified
-		if rc.status == http.StatusOK || rc.status == http.StatusNotModified {
+		// Cache the response if it's a 200 OK or 304 Not Modified and the request method is GET
+		if r.Method == http.MethodGet && (rc.status == http.StatusOK || rc.status == http.StatusNotModified) {
 			dateHeader := rc.headers.Get("Date")
 			expiresHeader := rc.headers.Get("Expires")
 			if dateHeader != "" && expiresHeader != "" {
@@ -111,10 +108,6 @@ func RequestHandler(proxy *httputil.ReverseProxy, url *url.URL, endpoint string,
 				}
 			}
 		}
-
-		// Return the status and body from upstream
-		//w.WriteHeader(rc.status)
-		//w.Write(rc.body)
 
 		limitRemain, _ := strconv.Atoi(rc.headers.Get("X-Esi-Error-Limit-Remain"))
 		limitReset, _ := strconv.Atoi(rc.headers.Get("X-Esi-Error-Limit-Reset"))
